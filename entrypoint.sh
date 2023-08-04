b064769924774b73d54a30dedde8b7a564179962
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-04 13:28:42 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,21 +8,21 @@ if [ -n "${GITHUB_WORKSPACE}" ]; then
 fi
 
 # default to latest
-TERRASEC_VERSION="latest"
+TFSECURITY_VERSION="latest"
 
-# if INPUT_TERRASEC_VERSION set and not latest
-if [[ -n "${INPUT_TERRASEC_VERSION}" && "$INPUT_TERRASEC_VERSION" != "latest" ]]; then
-  TERRASEC_VERSION="tags/${INPUT_TERRASEC_VERSION}"
+# if INPUT_TFSECURITY_VERSION set and not latest
+if [[ -n "${INPUT_TFSECURITY_VERSION}" && "$INPUT_TFSECURITY_VERSION" != "latest" ]]; then
+  TFSECURITY_VERSION="tags/${INPUT_TFSECURITY_VERSION}"
 fi
 
-# # Pull https://api.github.com/repos/khulnasoft-labs/terrasec/releases for the full list of releases. NOTE no trailing slash
-# wget --inet4-only -O - -q "$(wget --inet4-only -q https://api.github.com/repos/khulnasoft-labs/terrasec/releases/${TERRASEC_VERSION} -O - | grep -m 1 -o -E "https://.+?terrasec-linux-amd64" | head -n1)" > terrasec-linux-amd64
-# wget --inet4-only -O - -q "$(wget --inet4-only -q https://api.github.com/repos/khulnasoft-labs/terrasec/releases/${TERRASEC_VERSION} -O - | grep -m 1 -o -E "https://.+?terrasec_checksums.txt" | head -n1)" > terrasec.checksums
+# # Pull https://api.github.com/repos/khulnasoft-labs/tfsecurity/releases for the full list of releases. NOTE no trailing slash
+# wget --inet4-only -O - -q "$(wget --inet4-only -q https://api.github.com/repos/khulnasoft-labs/tfsecurity/releases/${TFSECURITY_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsecurity-linux-amd64" | head -n1)" > tfsecurity-linux-amd64
+# wget --inet4-only -O - -q "$(wget --inet4-only -q https://api.github.com/repos/khulnasoft-labs/tfsecurity/releases/${TFSECURITY_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsecurity_checksums.txt" | head -n1)" > tfsecurity.checksums
 
 # # pipe out the checksum and validate
-# grep terrasec-linux-amd64 terrasec.checksums > terrasec-linux-amd64.checksum
-# sha256sum -c terrasec-linux-amd64.checksum
-# install terrasec-linux-amd64 /usr/local/bin/terrasec
+# grep tfsecurity-linux-amd64 tfsecurity.checksums > tfsecurity-linux-amd64.checksum
+# sha256sum -c tfsecurity-linux-amd64.checksum
+# install tfsecurity-linux-amd64 /usr/local/bin/tfsecurity
 
 # if input vars file then add to arguments
 if [ -n "${INPUT_TFVARS_FILE}" ]; then
@@ -37,23 +37,23 @@ if [ -n "${INPUT_CONFIG_FILE}" ]; then
 fi
 
 # if any additional args included, add them on
-if [ -n "${INPUT_TERRASEC_ARGS}" ]; then
-  echo "Using specified args: ${INPUT_TERRASEC_ARGS}"
-  TERRASEC_ARGS_OPTION="${INPUT_TERRASEC_ARGS}"
+if [ -n "${INPUT_TFSECURITY_ARGS}" ]; then
+  echo "Using specified args: ${INPUT_TFSECURITY_ARGS}"
+  TFSECURITY_ARGS_OPTION="${INPUT_TFSECURITY_ARGS}"
 fi
 
 # if set,  all dirs to be included,
 if [ -n "${INPUT_FULL_REPO_SCAN}" ]; then
   echo "Forcing all directories to be scanned"
-  TERRASEC_ARGS_OPTION="--force-all-dirs ${TERRASEC_ARGS_OPTION}"
+  TFSECURITY_ARGS_OPTION="--force-all-dirs ${TFSECURITY_ARGS_OPTION}"
 fi
 
 
 # prime the sarif file with empty results
 echo {} > ${INPUT_SARIF_FILE}
 
-terrasec --soft-fail --out=${INPUT_SARIF_FILE} --format=sarif ${TERRASEC_ARGS_OPTION} ${CONFIG_FILE_OPTION} ${TFVARS_OPTION} "${INPUT_WORKING_DIRECTORY}" 
+tfsecurity --soft-fail --out=${INPUT_SARIF_FILE} --format=sarif ${TFSECURITY_ARGS_OPTION} ${CONFIG_FILE_OPTION} ${TFVARS_OPTION} "${INPUT_WORKING_DIRECTORY}" 
 
-terrasec_return="${PIPESTATUS[0]}" exit_code=$?
+tfsecurity_return="${PIPESTATUS[0]}" exit_code=$?
 
-echo "terrasec-return-code=${terrasec_return}" >> $GITHUB_OUTPUT
+echo "tfsecurity-return-code=${tfsecurity_return}" >> $GITHUB_OUTPUT
